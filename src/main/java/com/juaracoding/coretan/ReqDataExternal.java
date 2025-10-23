dca5a9a2cd8eb215961695e51290a73db54aed12
pollchihuy <poll.chihuy@gmail.com>
2025-10-23 22:18:55 +0700

    Springboot Project Day 4

---
+++ src/main/java/com/juaracoding/coretan/ReqDataExternal.java
@@ -0,0 +1,59 @@
+package com.juaracoding.coretan;
+
+/*
+IntelliJ IDEA 2024.1.4 (Ultimate Edition)
+Build #IU-241.18034.62, built on June 21, 2024
+@Author pollc a.k.a. Paul Christian
+Java Developer
+Created on 23/10/2025 19:32
+@Last Modified 23/10/2025 19:32
+Version 1.0
+*/
+public class ReqDataExternal {
+
+    private Long id;
+    private String nama;
+    private String alamat;
+    private String email;
+    private String tanggalLahir;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTanggalLahir() {
+        return tanggalLahir;
+    }
+
+    public void setTanggalLahir(String tanggalLahir) {
+        this.tanggalLahir = tanggalLahir;
+    }
+}

b8bfd81f2265b278c782e6a2a417506a4492d4d2
pollchihuy <poll.chihuy@gmail.com>
2025-11-05 21:53:34 +0700

    Spring Unit Testing Day 2

---
+++ src/main/java/com/juaracoding/repo/LogMenuRepo.java
@@ -0,0 +1,7 @@
+package com.juaracoding.repo;
+
+import com.juaracoding.model.LogMenu;
+import org.springframework.data.repository.CrudRepository;
+
+public interface LogMenuRepo extends CrudRepository<LogMenu, Long> {
+}
